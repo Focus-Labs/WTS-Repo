9797ffbb7ba8876316465b89bdbfcd4f003c8346
baba <babadawit@gmail.com>
2016-06-16 13:25:22 +0300

    add method and implement them

--- backend/src/main/java/com/focuslabs/wts/service/IUserService.java
+++ backend/src/main/java/com/focuslabs/wts/service/IUserService.java
@@ -1,12 +1,18 @@
 package com.focuslabs.wts.service;
 
+import com.focuslabs.wts.entity.User;
 import com.focuslabs.wts.vo.UserVo;
+
+import java.util.List;
 
 /**
  * Created by baba on 6/13/2016.
  */
 public interface IUserService {
 
-    UserVo getNumberOfUsers();
+    Long getNumberOfUsers();
+    List<User> getAllUsers();
+    User getUser(String userId);
+    User updateUser(UserVo user);
 
 }
