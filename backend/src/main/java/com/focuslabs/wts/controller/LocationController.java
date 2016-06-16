908514ce7dca540faf1e1c633cfb6658cd84be0e
baba <babadawit@gmail.com>
2016-06-16 13:21:24 +0300

    add two methods to get all the list and to get specific data

--- backend/src/main/java/com/focuslabs/wts/controller/LocationController.java
+++ backend/src/main/java/com/focuslabs/wts/controller/LocationController.java
@@ -1,17 +1,21 @@
 package com.focuslabs.wts.controller;
 
+import com.focuslabs.wts.entity.Location;
 import com.focuslabs.wts.service.ILocationService;
+import com.focuslabs.wts.vo.EventVo;
+import com.focuslabs.wts.vo.LocationVo;
 import com.focuslabs.wts.vo.UserVo;
+import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by baba on 6/13/2016.
@@ -27,18 +31,66 @@ public class LocationController {
     @ResponseBody
     @ApiOperation(value = "number of locations", notes = "return number of locations")
     @ApiResponses( value = {
-            @ApiResponse(code = 200, message = "Success", response = UserVo.class),
+            @ApiResponse(code = 200, message = "Success"),
             @ApiResponse(code = 401, message = "Unauthorized"),
             @ApiResponse(code = 404, message = "Not Found"),
             @ApiResponse(code = 500, message = "Failure")
     })
     public ResponseEntity<?> numberOfLocations() {
         try {
-            return new ResponseEntity<Object>(locationService.getNumberOdLocations(), HttpStatus.valueOf(200));
+            Long numberOfLocations = locationService.getNumberOdLocations();
+            return new ResponseEntity<Object>(numberOfLocations, HttpStatus.valueOf(200));
         } catch (Exception e) {
             e.printStackTrace();
             return new ResponseEntity<Object>(e.getMessage(), HttpStatus.valueOf(500));
         }
     }
+
+    @RequestMapping(value = "/allLocations",method = RequestMethod.GET, produces = "application/json;charset=UTF-8")
+    @ResponseBody
+    @ApiOperation(value = "get all locations", notes = "return all locations")
+    @ApiResponses( value = {
+            @ApiResponse(code = 200, message = "Success", response = LocationVo.class),
+            @ApiResponse(code = 401, message = "Unauthorized"),
+            @ApiResponse(code = 404, message = "Not Found"),
+            @ApiResponse(code = 500, message = "Failure")
+    })
+    public ResponseEntity<?> getAllEvents() {
+        try {
+            List<LocationVo> locations = new ArrayList<>();
+            locationService.getAllLocations().stream().forEach(l -> {
+                locations.add(new LocationVo(l.getId(),l.getName()));
+            });
+            return new ResponseEntity<Object>(locations, HttpStatus.valueOf(200));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<Object>(e.getMessage(), HttpStatus.valueOf(500));
+        }
+    }
+
+    @RequestMapping(value = "/location",method = RequestMethod.GET, produces = "application/json;charset=UTF-8")
+    @ResponseBody
+    @ApiOperation(value = "get location with specified location id", notes = "return the specified location")
+    @ApiResponses( value = {
+            @ApiResponse(code = 200, message = "Success", response = LocationVo.class),
+            @ApiResponse(code = 401, message = "Unauthorized"),
+            @ApiResponse(code = 404, message = "Not Found"),
+            @ApiResponse(code = 500, message = "Failure")
+    })
+    @ApiImplicitParam(name = "locationId", required = true, dataType = "string", paramType = "query", value = "unique location Id")
+    public ResponseEntity<?> getUser(@RequestParam(value = "locationId", required = true) String locationId) {
+        if(locationId != null && !locationId.isEmpty()) {
+            try {
+                Location l = locationService.getLocation(locationId);
+                LocationVo location = new LocationVo(l.getId(),l.getName());
+                return new ResponseEntity<Object>(location, HttpStatus.valueOf(200));
+            } catch (Exception e) {
+                e.printStackTrace();
+                return new ResponseEntity<Object>(e.getMessage(), HttpStatus.valueOf(500));
+            }
+        }
+        return new ResponseEntity<Object>("Parameter Error", HttpStatus.valueOf(500));
+    }
+
 
 }
--- backend/src/main/java/com/focuslabs/wts/controller/UserController.java
+++ backend/src/main/java/com/focuslabs/wts/controller/UserController.java
@@ -1,19 +1,21 @@
 package com.focuslabs.wts.controller;
 
+import com.focuslabs.wts.entity.User;
 import com.focuslabs.wts.service.IUserService;
+import com.focuslabs.wts.vo.EventVo;
+import com.focuslabs.wts.vo.LocationVo;
+import com.focuslabs.wts.vo.OrganizationVo;
 import com.focuslabs.wts.vo.UserVo;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 /**
  * Created by baba on 4/3/2016.
@@ -30,7 +32,7 @@ public class UserController {
     @ResponseBody
     @ApiOperation(value = "number of users", notes = "return number of users")
     @ApiResponses( value = {
-            @ApiResponse(code = 200, message = "Success", response = UserVo.class),
+            @ApiResponse(code = 200, message = "Success"),
             @ApiResponse(code = 401, message = "Unauthorized"),
             @ApiResponse(code = 404, message = "Not Found"),
             @ApiResponse(code = 500, message = "Failure")
@@ -43,4 +45,85 @@ public class UserController {
             return new ResponseEntity<Object>(e.getMessage(), HttpStatus.valueOf(500));
         }
     }
+
+    @RequestMapping(value = "/allUsers",method = RequestMethod.GET, produces = "application/json;charset=UTF-8")
+    @ResponseBody
+    @ApiOperation(value = "get all users", notes = "return all users")
+    @ApiResponses( value = {
+            @ApiResponse(code = 200, message = "Success", response = UserVo.class),
+            @ApiResponse(code = 401, message = "Unauthorized"),
+            @ApiResponse(code = 404, message = "Not Found"),
+            @ApiResponse(code = 500, message = "Failure")
+    })
+    public ResponseEntity<?> getAllUsers() {
+        try {
+            List<UserVo> users = new ArrayList<>();
+            userService.getAllUsers().stream().forEach(u -> {
+                UserVo user = new UserVo(u.getId(), u.getEmail(), u.getPassword(), u.getFirstName(), u.getLastName(), u.getAboutMe(), u.getEducation());
+                u.getEvents().stream().forEach(e -> {
+                    user.getEvents().add(new EventVo(e.getId(), e.getTitle(), e.getDate(),e.getLength(), e.getShort_desc(), e.getLong_desc(), e.getWhere_about(), e.getEvent_icon_Location(), e.getEvent_pic_Location()));
+                });
+                u.getOrganizations().stream().forEach(o -> {
+                    UserVo admin = new UserVo(o.getAdmin().getId(), o.getAdmin().getEmail(), o.getAdmin().getPassword(), o.getAdmin().getFirstName(), o.getAdmin().getLastName(), o.getAdmin().getAboutMe(), o.getAdmin().getEducation());
+                    LocationVo location = new LocationVo(o.getAddress().getId(), o.getAddress().getName());
+                    user.getOrganizations().add(new OrganizationVo(o.getId(), o.getName(), o.getHomepage(), location, admin));
+                });
+                users.add(user);
+            });
+            return new ResponseEntity<Object>(users, HttpStatus.valueOf(200));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<Object>(e.getMessage(), HttpStatus.valueOf(500));
+        }
+    }
+
+    @RequestMapping(value = "/user",method = RequestMethod.GET, produces = "application/json;charset=UTF-8")
+    @ResponseBody
+    @ApiOperation(value = "get user with specified user id", notes = "return the specified user")
+    @ApiResponses( value = {
+            @ApiResponse(code = 200, message = "Success", response = UserVo.class),
+            @ApiResponse(code = 401, message = "Unauthorized"),
+            @ApiResponse(code = 404, message = "Not Found"),
+            @ApiResponse(code = 500, message = "Failure")
+    })
+    @ApiImplicitParam(name = "userId", required = true, dataType = "string", paramType = "query", value = "unique user Id")
+    public ResponseEntity<?> getUser(@RequestParam(value = "userId", required = true) String userId) {
+        if(userId != null && !userId.isEmpty()) {
+            try {
+                User u = userService.getUser(userId);
+                UserVo user = new UserVo(u.getId(),u.getEmail(),u.getPassword(),u.getFirstName(),u.getLastName(),u.getAboutMe(),u.getEducation());
+                return new ResponseEntity<Object>(user, HttpStatus.valueOf(200));
+            } catch (Exception e) {
+                e.printStackTrace();
+                return new ResponseEntity<Object>(e.getMessage(), HttpStatus.valueOf(500));
+            }
+        }
+        return new ResponseEntity<Object>("Parameter Error", HttpStatus.valueOf(500));
+    }
+
+    @RequestMapping(value = "/update",method = RequestMethod.PUT, produces = "application/json;charset=UTF-8")
+    @ResponseBody
+    @ApiOperation(value = "update user", notes = "return the modified user")
+    @ApiResponses( value = {
+            @ApiResponse(code = 200, message = "Success", response = UserVo.class),
+            @ApiResponse(code = 401, message = "Unauthorized"),
+            @ApiResponse(code = 404, message = "Not Found"),
+            @ApiResponse(code = 500, message = "Failure")
+    })
+//    @ApiImplicitParam(name = "userId", required = true, dataType = "string", paramType = "query", value = "unique user Id")
+    public ResponseEntity<?> updateUser(@RequestBody UserVo user) {
+        if(user != null) {
+            try {
+                User newUser = userService.updateUser(user);
+                return new ResponseEntity<Object>(newUser, HttpStatus.valueOf(200));
+            } catch (Exception e) {
+                e.printStackTrace();
+                return new ResponseEntity<Object>(e.getMessage(), HttpStatus.valueOf(500));
+            }
+        }
+        return new ResponseEntity<Object>("Parameter Error", HttpStatus.valueOf(500));
+    }
+
+
+
 }
